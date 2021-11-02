b2ed514db6e3a4b0362be04eda6c2bd488e39fbc
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-11-01 21:34:07 -0300

    inclui stats e ranking

---
+++ ranking.php
@@ -0,0 +1,67 @@
+<?php
+
+    session_start();
+
+	require_once 'config/config.php';
+
+	$ranking = "SELECT ra.pontuacao, us.nickname
+	FROM (ranking ra 
+		  JOIN 
+          users us
+          ON 
+          ra.id_usuario = us.id_user) 
+	ORDER BY pontuacao DESC LIMIT 10";      
+
+	$ran = $mysql_db->query($ranking) or die($mysql_db->error);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hall da Fama</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-qdQEsAI45WFCO5QwXBelBe1rR9Nwiss4rGEqiszC+9olH1ScrLrMQr1KmDR964uZ" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./img/12130brain_109577.ico" />
+	<style>
+        .wrapper{ 
+        	width: 1200px; 
+        	padding: 100px;  
+        }
+        .wrapper h1 {text-align: center}
+        .wrapper form .form-group span {color: red;}
+	</style>
+</head>
+<body>
+	<main>
+		<section class="container wrapper">
+				<h1 class="display-5">Hall da Fama</h1>
+                <br>
+
+                <?php $pos = 1; ?>
+
+                <table class="table" border="1">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col">POSIÇÃO</th>
+                        <th scope="col">PONTUAÇÃO</th>
+                        <th scope="col">NICKNAME DO JOGADOR</th>
+                      </tr>       
+                    </td><?php while($dado = $ran->fetch_array()) { ?> 
+                        <tr> 
+                            <th><?php echo $pos; ?></th>
+                            <th><?php echo $dado['pontuacao']; ?></th> 
+                            <th><?php echo $dado['nickname']; ?></th>
+                            <?php $pos++; ?>
+                        </tr> 
+                    <?php } ?> 
+                </table>
+
+                <a class="btn btn-block btn-link bg-light" href="welcome.php">Sair</a>
+                
+		</section>
+	</main>
+</body>
+</html>
+
+
---
+++ stats.php
@@ -0,0 +1,67 @@
+<?php
+
+    session_start();
+
+	require_once 'config/config.php';
+
+    $param_id = $_SESSION['id_user'];
+
+	$consulta = "SELECT * FROM stats WHERE id_user_stats = '$param_id'";
+    $cons = $mysql_db->query($consulta) or die($mysql_db->error);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Stats</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-qdQEsAI45WFCO5QwXBelBe1rR9Nwiss4rGEqiszC+9olH1ScrLrMQr1KmDR964uZ" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./img/12130brain_109577.ico" />
+	<style>
+        .wrapper{ 
+        	width: 1400px; 
+        	padding: 100px; 
+        }
+        .wrapper h1 {text-align: center}
+        .wrapper form .form-group span {color: red;}
+	</style>
+</head>
+<body>
+	<main>
+		<section class="container wrapper">
+            <h1 class="display-5"><strong>Stats de <?php echo $_SESSION['nickname']; ?></strong></h1>
+                <br>
+
+                <?php $pos = 1; ?>
+
+                <table class="table" border="1">
+                    <thead class="thead-light">
+                      <tr>
+                        <th scope="col">Nº DE PARTIDAS JOGADAS</th>
+                        <th scope="col">Nº DE PERGUNTAS RESPONDIDAS</th>
+                        <th scope="col">Nº DE DERROTAS POR ERRO</th>
+                        <th scope="col">Nº DE DERROTAS POR PARADA</th>
+                        <th scope="col">Nº DE ELIMINAÇÕES DE DUAS ALTERNATIVAS</th>
+                        <th scope="col">PREMIAÇÃO TOTAL ACUMULADA</th>
+                      </tr>       
+                    </td><?php while($dado = $cons->fetch_array()) { ?> 
+                        <tr> 
+                            <th><?php echo $dado['n_partidas_jogadas']; ?></th>
+                            <th><?php echo $dado['n_tot_perg_resp']; ?></th>
+                            <th><?php echo $dado['n_derr_erro']; ?></th>
+                            <th><?php echo $dado['n_derr_parada']; ?></th>
+                            <th><?php echo $dado['n_util_eli_duas_altern']; ?></th>
+                            <th><?php echo $dado['premio_total']; ?></th>
+                        </tr> 
+                    <?php } ?> 
+                </table>
+
+                <a class="btn btn-block btn-link bg-light" href="welcome.php">Sair</a>
+                
+		</section>
+	</main>
+</body>
+</html>
+
+
