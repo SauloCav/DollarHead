90a8649e6930f67cc35965f100c88785409c1493
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-01-19 08:11:47 -0300

    Update init.php

--- Game/init.php
+++ Game/init.php
@@ -18,7 +18,7 @@
 	$_SESSION["quest_5"] = $MyList->findObject(4);
 	$_SESSION["quest_6"] = $MyList->findObject(5);
 	$_SESSION["quest_7"] = $MyList->findObject(6);
-	$_SESSION['quest'] = $_SESSION["quest_1"];
+	$_SESSION['quest'] = "quest_1";
 
 ?>
 
