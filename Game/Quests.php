821e1ab7d848695c2943086de6ebef9460150373
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-02-12 19:54:39 -0300

    modifica telas e limpa o código

--- Game/Quests.php
+++ Game/Quests.php
@@ -13,10 +13,6 @@
 	<title>Jogo</title>
 	<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/cosmo/bootstrap.min.css" rel="stylesheet" integrity="sha384-qdQEsAI45WFCO5QwXBelBe1rR9Nwiss4rGEqiszC+9olH1ScrLrMQr1KmDR964uZ" crossorigin="anonymous">
 	<link rel="shortcut icon" href="../img/12130brain_109577.ico" />
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="./styles/game.css">
 	<style>
 		body {
                 background-image: url("../img/photo-1560780552-ba54683cb263.jpeg");
