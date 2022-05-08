94671e50858072f21544d97db2d38d68c803a903
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-05-08 17:38:47 -0300

    finaliza a hierarquização das questões na parte do game

--- Game/Quests.php
+++ Game/Quests.php
@@ -1,8 +1,6 @@
 <?php
-
 	session_start();
 	require_once 'linked_list.php';
-
 ?>
 
 <!DOCTYPE html>
--- Game/linked_list.php
+++ Game/linked_list.php
@@ -37,9 +37,15 @@
 
 	};
 
-	$quests = "SELECT * FROM questoes_respostas WHERE id_questao IS NOT NULL AND valida = 'v'"; 
+	$quests = "SELECT * FROM questoes_respostas WHERE id_questao IS NOT NULL AND valida = 'v' AND indice_dif = '1'"; 
     $ques = $mysql_db->query($quests) or die($mysql_db->error);
-    $row = mysqli_fetch_all($ques);
+    $row_dif_1 = mysqli_fetch_all($ques);
+    $quests = "SELECT * FROM questoes_respostas WHERE id_questao IS NOT NULL AND valida = 'v' AND indice_dif = '2'"; 
+    $ques = $mysql_db->query($quests) or die($mysql_db->error);
+    $row_dif_2 = mysqli_fetch_all($ques);
+    $quests = "SELECT * FROM questoes_respostas WHERE id_questao IS NOT NULL AND valida = 'v' AND indice_dif = '3'"; 
+    $ques = $mysql_db->query($quests) or die($mysql_db->error);
+    $row_dif_3 = mysqli_fetch_all($ques);
     
     $_SESSION["prize"] = 0;
 
@@ -48,43 +54,43 @@
     $MyList = new LinkedList();
 
     $first = new Node();
-    $first->data = $row[0];
+    $first->data = $row_dif_1[0];
     $first->next = null;
     $first->num = 0;
     $MyList->head = $first;
 
     $second = new Node();
-    $second->data = $row[1];
+    $second->data = $row_dif_1[1];
     $second->next = null;
     $second->num = 1;
     $first->next = $second;
 
     $third = new Node();
-    $third->data = $row[2];
+    $third->data = $row_dif_2[2];
     $third->next = null;
     $third->num = 2;
     $second->next = $third;
 
     $fourth = new Node();
-    $fourth->data = $row[3];
+    $fourth->data = $row_dif_2[3];
     $fourth->next = null;
     $fourth->num = 3;
     $third->next = $fourth;
 
     $fifth = new Node();
-    $fifth->data = $row[4];
+    $fifth->data = $row_dif_2[4];
     $fifth->next = null;
     $fifth->num = 4;
     $fourth->next = $fifth;
 
     $sixth = new Node();
-    $sixth->data = $row[5];
+    $sixth->data = $row_dif_3[5];
     $sixth->next = null;
     $sixth->num = 5;
     $fifth->next = $sixth;
 
     $seventh = new Node();
-    $seventh->data = $row[6];
+    $seventh->data = $row_dif_3[6];
     $seventh->next = null;
     $seventh->num = 6;
     $sixth->next = $seventh;
