7b877bc23e66c5357f145fae4b7eac4e587bb582
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2022-06-03 19:31:34 -0300

    Inicia adição do gráfico no ranking

--- ranking.php
+++ ranking.php
@@ -14,6 +14,27 @@
 
 	$ran = $mysql_db->query($ranking) or die($mysql_db->error);
 
+    $i = 0;
+
+    while($dado = $ran->fetch_array()) { 
+
+        $dataPoints[$i] = array("x"=> $i, "y"=> (int)$dado['pontuacao']);
+        $i = $i + 1;
+
+    }
+
+    print_r($dataPoints);
+
+    $ranking = "SELECT ra.pontuacao, us.nickname, ra.data_pont
+	FROM (ranking ra 
+		  JOIN 
+          users us
+          ON 
+          ra.id_usuario = us.id_user) 
+	ORDER BY pontuacao DESC LIMIT 10";      
+
+	$ran = $mysql_db->query($ranking) or die($mysql_db->error);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,52 +49,82 @@
             font-family: 'Poppins', sans-serif;
             position: relative;
             top: 20%;
-            transform: translateY(8%); 
+            transform: translateY(7%); 
         }
         .wrapper{ 
-        	width: 1800px; 
-        	padding: 20px; 
+        	width: 1400px; 
+            text-align: left;
         }
-        .wrapper h1 {
+        #internalDivStyle {
+            width: 800px;
+            text-align: right;
+        }
+        h1 {
 			text-align: center;
 		}
-        .wrapper form .form-group span {color: red;}
         .table {
 			text-align: center;  
 		}
-        .wrapper form .form-group span {color: red;}
+        #globalDivStyle {
+            width: 800px;
+        }
 	</style>
+    <script>
+        window.onload = function () {
+        
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light1", // "light1", "light2", "dark1", "dark2"
+            title: {
+                text: "Top 10 melhores pontuações"
+            },
+            axisY: {
+                title: "Pontuação" 
+            },
+            data: [{
+                type: "column",
+                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+            }]
+        });
+        chart.render();
+        
+        }
+    </script>
 </head>
 <body>
 	<main>
-		<section class="container wrapper">
-				<h1 class="display-5"><strong>Hall da Fama<strong></h1>
-                <br>
+        <h1 class="display-5"><strong>Hall da Fama<strong></h1>
+        <br>
+        <div id="globalDivStyle">
+            <section class="container wrapper">
 
-                <?php $pos = 1; ?>
+                    <?php $pos = 1; ?>
 
-                <table class="table" border="3">
-                    <thead class="thead-light">
-                      <tr>
-                        <th scope="col">Posição</th>
-                        <th scope="col">Nickname</th>
-                        <th scope="col">Pontuação</th>
-                        <th scope="col">Data e Hora da Jogada</th>
-                      </tr>       
-                    </td><?php while($dado = $ran->fetch_array()) { ?> 
-                        <tr> 
-                            <th><?php echo $pos; ?></th>
-                            <th><?php echo $dado['nickname']; ?></th>
-                            <th><?php echo $dado['pontuacao']; ?></th> 
-                            <th><?php echo $dado['data_pont']; ?></th>
-                            <?php $pos++; ?>
-                        </tr> 
-                    <?php } ?> 
-                </table>
-
-                <a class="btn btn-block btn-link bg-light" href="welcome.php">Sair</a>
-                
-		</section>
+                    <table class="table" border="3">
+                        <thead class="thead-light">
+                        <tr>
+                            <th scope="col">Posição</th>
+                            <th scope="col">Nickname</th>
+                            <th scope="col">Pontuação</th>
+                            <th scope="col">Data e Hora da Jogada</th>
+                        </tr>       
+                        </td><?php while($dado = $ran->fetch_array()) { ?> 
+                            <tr> 
+                                <th><?php echo $pos; ?></th>
+                                <th><?php echo $dado['nickname']; ?></th>
+                                <th><?php echo $dado['pontuacao']; ?></th> 
+                                <th><?php echo $dado['data_pont']; ?></th>
+                                <?php $pos++; ?>
+                            </tr> 
+                        <?php } ?> 
+                    </table>      
+            </section>
+            <div id="internalDivStyle">
+                <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+            </div>
+        </div>
+        <a class="btn btn-block btn-link bg-light" href="welcome.php">Sair</a>
 	</main>
 </body>
 </html>
