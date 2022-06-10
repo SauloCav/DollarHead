6cd05d2657a417ca8e87df1a8c8e22e700381d9c
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-06-10 10:08:30 -0300

    Update englishDatabase.sql

--- Documentation/dataBase/englishDatabase.sql
+++ Documentation/dataBase/englishDatabase.sql
@@ -1,14 +1,4 @@
-CREATE TABLE questions_answer (
-    id_question INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    question varchar(100) NOT NULL,
-    rigth_answer varchar(50) NOT NULL,
-    answer_a varchar(50) NOT NULL,
-	answer_b varchar(50) NOT NULL,
-	answer_c varchar(50) NOT NULL,
-    difficulty_index INT NOT NULL,
-    topic_question varchar(20) NOT NULL,
-	validate enum ('v','i') NOT NULL
-);
+
 
 CREATE TABLE denounce_validation (
     id_den_val INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -20,79 +10,71 @@ CREATE TABLE denounce_validation (
 	FOREIGN KEY(id_question) references questions_answer(id_question)
 );
 
-CREATE TABLE stats (
-	id_stats INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    num_games_played INT NOT NULL,
-	award INT NOT NULL,
-	num_use_two_alteratives INT NOT NULL,
-    num_defeats_error INT NOT NULL,
-	num_defeats_stop INT NOT NULL,
-	num_contributions INT NOT NULL,
-    user_level VARCHAR(50) NOT NULL,
-    id_user_stats INT NOT NULL,
-	FOREIGN KEY(id_user_stats) references users(id_user)
-);
-
-CREATE TABLE ranking (
-    id_ranking INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    punctuation REAL NOT NULL,
-    date_punctuation DATETIME DEFAULT CURRENT_TIMESTAMP,
-	id_user INT NOT NULL,
-	FOREIGN KEY(id_user references users(id_user)
-);
-
+	
 	
 create database quizGame;
 
-CREATE TABLE users (
-    id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    nickname VARCHAR(20) NOT NULL
+CREATE TABLE Users (
+	idUser INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	nickname VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE stats (
-	id_estats INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    n_partidas_jogadas INT NOT NULL,
-	premio_total INT NOT NULL,
-	n_util_eli_duas_altern INT NOT NULL,
-    n_derr_erro INT NOT NULL,
-	n_derr_parada INT NOT NULL,
-	num_contributions INT NOT NULL,
-    user_level VARCHAR(50) NOT NULL,
-    id_user_stats INT NOT NULL,
-	FOREIGN KEY(id_user_stats) references users(id_user)
+CREATE TABLE Stats (
+	idStats INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	numRoundsPlayed INT NOT NULL,
+	totalPrize INT NOT NULL,
+	numEliminationOfAlternatives INT NOT NULL,
+	numLossesByMistake INT NOT NULL,
+	numLossesByStop INT NOT NULL,
+	numContributions INT NOT NULL,
+	userLevel VARCHAR(50) NOT NULL,
+	idUserStats INT NOT NULL,
+	FOREIGN KEY(idUserStats) references users(idUser)
 );
 
-CREATE TABLE ranking (
-    id_ranking INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    pontuacao REAL NOT NULL,
-    data_pont DATETIME DEFAULT CURRENT_TIMESTAMP,
-	id_usuario INT NOT NULL,
-	FOREIGN KEY(id_usuario) references users(id_user)
+CREATE TABLE Ranking (
+	idRanking INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	score REAL NOT NULL,
+	scoreDateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+	idUserRanking INT NOT NULL,
+	FOREIGN KEY(idUserRanking) references users(idUser)
 );
 
-CREATE TABLE latest_scores (
-	id_latest_scores INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE LatestScores (
+	idLatestScores INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	prize01 INT,
 	prize02 INT,
 	prize03 INT,
 	prize04 INT,
 	prize05 INT,
-	id_user_latest_scores INT NOT NULL,
-	FOREIGN KEY(id_user_latest_scores) references users(id_user)
+	idUserLatestScores INT NOT NULL,
+	FOREIGN KEY(idUserLatestScores) references users(idUser)
 );
 
-CREATE TABLE questoes_respostas (
-    id_questao INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    pergunta varchar(100) NOT NULL,
-    resp_correta varchar(50) NOT NULL,
-    resp_a varchar(50) NOT NULL,
-	resp_b varchar(50) NOT NULL,
-	resp_c varchar(50) NOT NULL,
-    indice_dif INT NOT NULL,
-    quest_topico varchar(20) NOT NULL,
+CREATE TABLE Questions (
+	idQuestion INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	question varchar(100) NOT NULL,
+	rigthAnswer varchar(50) NOT NULL,
+	answerA varchar(50) NOT NULL,
+	answerB varchar(50) NOT NULL,
+	answerC varchar(50) NOT NULL,
+	indice_dif INT NOT NULL,
+	quest_topico varchar(20) NOT NULL,
 	valida enum ('v','i') NOT NULL
+);
+
+CREATE TABLE questions_answer (
+    id_question INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    question varchar(100) NOT NULL,
+    rigth_answer varchar(50) NOT NULL,
+    answer_a varchar(50) NOT NULL,
+	answer_b varchar(50) NOT NULL,
+	answer_c varchar(50) NOT NULL,
+    difficulty_index INT NOT NULL,
+    topic_question varchar(20) NOT NULL,
+	validate enum ('v','i') NOT NULL
 );
 
 CREATE TABLE denuncia_validacao (
